c8d13817388b245ba332428c496eba0a74b762ca
Joseph Matthias Goh <govtech@joeir.net>
2016-11-08 11:13:36 +0800

    Set up password mechanism for storing ID and password.

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 const restify = require('restify');
 const builder = require('botbuilder');
+const crypt = require('./crypt');
 const ConfigGuardian = require('config-guardian');
 const config = ConfigGuardian({ ignore: ['node_modules'], refresh: true });
-
 const server = restify.createServer();
-console.log(config);
+let applicationPassword = null;
+try {
+  applicationPassword = fs.readFileSync(path.join(__dirname, '/PASSWORD')).toString();
+} catch(ex) {
+  switch(ex.code) {
+    case 'ENOENT':
+      console.info('Create a file named `PASSWORD` in the project root and enter password as it\'s content.');
+      process.exit();
+    break;
+    default:
+      throw ex;
+  }
+}
+config.botCredentials.appPassword = crypt.decrypt(config.botCredentials.appPassword, applicationPassword);
+config.botCredentials.appId = crypt.decrypt(config.botCredentials.appId, applicationPassword);
 const connector = (config.environment === 'development') ?
   new builder.ConsoleConnector().listen() :
   new builder.ChatConnector(config.botCredentials);
--- config/server.config.js
+++ config/server.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  botCredentials: {
+    appId: '33571b702ec9b7f8b18afe64d035aa1b8db5feac076ccb58342f878798da81502d5f3991',
+    appPassword: '42163447209bad8fd2c19402ad6ba91ddae6ab89563787'
+  },
   environment: (process.env.NODE_ENV || 'development')
 };
