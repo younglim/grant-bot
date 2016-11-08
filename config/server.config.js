4f04d714b09b75dca4f3a0efe89522c0e0730745
Joseph Matthias Goh <govtech@joeir.net>
2016-11-08 11:36:33 +0800

    Using MICROSOFT_APP_* environment variables in configuration.

--- config/server.config.js
+++ config/server.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   botCredentials: {
-    appId: '33571b702ec9b7f8b18afe64d035aa1b8db5feac076ccb58342f878798da81502d5f3991',
-    appPassword: '42163447209bad8fd2c19402ad6ba91ddae6ab89563787'
+    appId: process.env.MICROSOFT_APP_ID || '33571b702ec9b7f8b18afe64d035aa1b8db5feac076ccb58342f878798da81502d5f3991',
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || '42163447209bad8fd2c19402ad6ba91ddae6ab89563787'
   },
   environment: (process.env.NODE_ENV || 'development')
 };
