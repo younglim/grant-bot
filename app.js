4a1485b11d33816c39d422adba785e31376323bb
Zui Young LIM <lim_zui_young@ida.gov.sg>
2016-11-08 13:25:15 +0800

    Test

--- app.js
+++ app.js
@@ -2,24 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const restify = require('restify');
 const builder = require('botbuilder');
-const crypt = require('./crypt');
 const ConfigGuardian = require('config-guardian');
 const config = ConfigGuardian({ ignore: ['node_modules'], refresh: true });
 const server = restify.createServer();
-let applicationPassword = null;
-try {
-  applicationPassword = (process.env.MICROSOFT_APP_ID && process.env.MICROSOFT_APP_PASSWORD) ?
-    '' : fs.readFileSync(path.join(__dirname, '/PASSWORD')).toString();
-} catch(ex) {
-  switch(ex.code) {
-    case 'ENOENT':
-      console.info('Create a file named `PASSWORD` in the project root and enter password as it\'s content.');
-      process.exit();
-    break;
-    default:
-      throw ex;
-  }
-}
+
 config.botCredentials.appId = process.env.MICROSOFT_APP_ID ? process.env.MICROSOFT_APP_ID : crypt.decrypt(config.botCredentials.appId, applicationPassword);
 config.botCredentials.appPassword = process.env.MICROSOFT_APP_PASSWORD ? process.env.MICROSOFT_APP_PASSWORD : crypt.decrypt(config.botCredentials.appPassword, applicationPassword);
 const connector = (config.environment === 'development') ?
