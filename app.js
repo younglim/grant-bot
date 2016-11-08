6e4f077e4f51c922726dd835af828dcd464d379d
Joseph Matthias Goh <govtech@joeir.net>
2016-11-08 14:04:27 +0800

    Added superagent and adding a link to LUIS.

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ config.botCredentials.appPassword = process.env.MICROSOFT_APP_PASSWORD;
 config.luisCredentials.id = process.env.MICROSOFT_LUIS_ID;
 config.luisCredentials.key = process.env.MICROSOFT_LUIS_KEY;
 
-// "https://api.projectoxford.ai/luis/v1/application?id=${config.luisCredentials.id}&subscription-key=${config.luisCredentials.key}"
+var model = "https://api.projectoxford.ai/luis/v1/application?id=${config.luisCredentials.id}&subscription-key=${config.luisCredentials.key}";
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+dialog.onDefault(builder.DialogAction.send("It went through"));
 
 /**
  * START CONTROLLER
@@ -28,9 +31,7 @@ var connector = (config.environment === 'development') ?
   new builder.ConsoleConnector().listen() :
   new builder.ChatConnector(config.botCredentials);
 var bot = new builder.UniversalBot(connector);
-bot.dialog('/', function (session) {
-    session.send("Hello Worasdasdasdsald!");
-});
+bot.dialog('/', dialog);
 if(config.environment === 'production') {
   server.post('/api/messages', connector.listen());
   server.listen(process.env.port || process.env.PORT || 3978, function () {
