63667b50d3b8f61cab4e605210955bdcdc5900fb
Zui Young LIM <lim_zui_young@ida.gov.sg>
2016-11-08 13:43:39 +0800

    Hello World update

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
-const fs = require('fs');
-const path = require('path');
 const restify = require('restify');
 const builder = require('botbuilder');
-const config = ConfigGuardian({ ignore: ['node_modules'], refresh: true });
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
+
 const server = restify.createServer();
 
 config.botCredentials.appId = process.env.MICROSOFT_APP_ID ? process.env.MICROSOFT_APP_ID : crypt.decrypt(config.botCredentials.appId, applicationPassword);
 config.botCredentials.appPassword = process.env.MICROSOFT_APP_PASSWORD ? process.env.MICROSOFT_APP_PASSWORD : crypt.decrypt(config.botCredentials.appPassword, applicationPassword);
-const connector = (config.environment === 'development') ?
-  new builder.ConsoleConnector().listen() :
-  new builder.ChatConnector(config.botCredentials);
+
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
 const bot = new builder.UniversalBot(connector);
 bot.dialog('/', function (session) {
     session.send("Hello Worasdasdasdsald! :D");
 });
-if(config.environment === 'production') {
-  server.post('/api/messages', connector.listen());
-  server.listen(process.env.port || process.env.PORT || 3978, function () {
+
+server.post('/api/messages', connector.listen());
+server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url); 
-  });
-}
+});
