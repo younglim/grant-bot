9170d550b30b9a8aaec89f294e0f095591d65280
Joseph Matthias Goh <govtech@joeir.net>
2016-11-08 10:56:22 +0800

    Removing bot credentials from configuration.

--- config/server.config.js
+++ config/server.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  botCredentials: {
-    appId: '60cfd3b0-8249-4d39-a308-ad285a5b875d',
-    appPassword: 'VSTcPX26f3ckGbVVdJL3f68'
-  },
   environment: (process.env.NODE_ENV || 'development')
 };
