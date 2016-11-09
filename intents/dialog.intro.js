dc373183b1679f564b7162c0cf503c1f529e54aa
Joseph Matthias Goh <govtech@joeir.net>
2016-11-09 09:22:53 +0800

    Adding intro intent.

---
+++ intents/dialog.intro.js
@@ -0,0 +1,12 @@
+module.exports = {
+  label: 'GrantBot Intro',
+  callbackProvider: (builder) => {
+    return [
+      function(session, args, next) {
+        session.send('1');
+        next();
+      },
+      builder.DialogAction.send('Hello! I\'m Grant. I can help answer your questions about applying for grants on Business Grants Portal.')
+    ];
+  }
+};
