26cc2f0b0c728c71d5ce4fe08d14b6f68da869e8
Joseph Matthias Goh <govtech@joeir.net>
2016-11-09 13:16:28 +0800

    Testing out sendTyping() with 'Time to get grant' intent.

--- intents/dialog.time.to.get.grant.js
+++ intents/dialog.time.to.get.grant.js
@@ -3,9 +3,15 @@ module.exports = {
   callbackProvider: (builder) => {
     return [
       function(session, args, next) {
-        next();
+        session.sendTyping();
+        setTimeout(function() {
+          session.send('After you submit your application, the agency giving out the grant will need 2-4 weeks to process it.');
+          session.sendTyping();
+        }, 1000);
+        setTimeout(function() {
+          session.send('If your grant is approved, you can submit your claim to get your grant money!');
+        }, 5000);
       },
-      builder.DialogAction.send('After you submit your application, the agency giving out the grant will need 2-4 weeks to process it. If your grant is approved, you can submit your claim to get your grant money.')
     ];
   }
 };
