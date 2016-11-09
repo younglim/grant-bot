5a723f2be053e1bcc9c45e624c5530e1b9722205
Zui Young LIM <lim_zui_young@ida.gov.sg>
2016-11-09 10:57:17 +0800

    Session send ocr response.

--- app.js
+++ app.js
@@ -67,9 +67,7 @@ bot.dialog('/uploadImage', [
           url: attachment.contentUrl,
           language: 'en'
         }).then(function (response) {
-          var msg =  builder.Message(session)
-          .ntext(response.body, response.body);
-          session.endDialog(msg);
+          session.send(response.body);
         });
     });
     
