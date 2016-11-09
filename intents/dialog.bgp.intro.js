217decfc80dccd7df179e74632ebfee9782b9f93
Kavan Koh <kavanohk@gmail.com>
2016-11-09 11:38:47 +0800

    Update intro intent

--- intents/dialog.bgp.intro.js
+++ intents/dialog.bgp.intro.js
@@ -3,9 +3,25 @@ module.exports = {
   callbackProvider: (builder) => {
     return [
       function(session, args, next) {
+        session.send('Business Grants Portal brings government grants for businesses into one place, so it\'s easier to find and apply for the grants you need. The grant support you receive will depend on your situation.');
         next();
       },
-      builder.DialogAction.send('Business Grants Portal brings government grants for businesses into one place, so it\'s easier to find and apply for the grants you need. The grant support you receive will depend on your situation.')
+      function(session, args, next) {
+        builder.Prompts.choice(session,'Would you like to know how to apply for a grant?', 'Yes|No');
+      },
+      function(session, result, next) {
+        switch(result.response.entity) {
+          case 'Yes':
+            session.send('To apply for a grant, you need a CorpPass account. Does your company have a CorpPass administrator?');
+            break;
+          case 'No':
+            session.send('What else can I help with today?');
+            break;
+        }
+        next();
+      }
     ];
   }
 };
+
+// What is business grants portal?
