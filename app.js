1092086b28c3a46cab867a23f704d4772ed192e6
Zui Young LIM <lim_zui_young@ida.gov.sg>
2016-11-09 14:10:23 +0800

    Check response regions length.

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ bot.dialog('/uploadImage', [
           language: 'en'
         }).then(function (response) {
           
-          if (typeof response.regions != 'undefined' && typeof response.lines != 'undefined') {
+          if ((typeof response.regions !== 'undefined') && (response.regions.length > 0) && (typeof response.regions[0].lines !== 'undefined')) {
 
             var data = response.regions[0];
             var ocrText = '';
