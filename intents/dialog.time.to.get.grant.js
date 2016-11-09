cdba08e436423094fe62a621f77c6b6434dc8248
Joseph Matthias Goh <govtech@joeir.net>
2016-11-09 15:16:26 +0800

    Refactors and completes intent 'Time to get grant'.

--- intents/dialog.time.to.get.grant.js
+++ intents/dialog.time.to.get.grant.js
@@ -1,17 +1,23 @@
+function getFlow(builder) {
+  return [
+    function(session, args, next) {
+      session.sendTyping();
+      setTimeout(function() {
+        session.send('After you submit your application, the agency giving out the grant will need 2-4 weeks to process it...');
+        session.sendTyping();
+      }, 1000);
+      setTimeout(function() {
+        session.send('If your grant is approved, you can submit your claim to get your grant money!');
+        session.endDialog();
+      }, 3000);
+    },
+  ];
+}
+
 module.exports = {
   label: 'Time to get grant',
-  callbackProvider: (builder) => {
-    return [
-      function(session, args, next) {
-        session.sendTyping();
-        setTimeout(function() {
-          session.send('After you submit your application, the agency giving out the grant will need 2-4 weeks to process it.');
-          session.sendTyping();
-        }, 1000);
-        setTimeout(function() {
-          session.send('If your grant is approved, you can submit your claim to get your grant money!');
-        }, 5000);
-      },
-    ];
+  callbackProvider: (builder, bot) => {
+    bot.dialog('/time-to-get-grant', getFlow(builder));
+    return getFlow(builder);
   }
 };
