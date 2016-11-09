bd9d35b8b3e33038e79cd5892db7b6aadaf38c86
Kia Hwee <chew_kiahwee@hotmail.com>
2016-11-09 10:29:07 +0800

    test prompts

--- intents/dialog.none.js
+++ intents/dialog.none.js
@@ -3,7 +3,14 @@ module.exports = {
   callbackProvider: (builder) => {
     return [
       function(session, args, next) {
-        session.send('1');
+        session.send("CrunchBot can answer questions about a handfull of tech companies. Here are some of the things you can ask:\n\n" +
+        "* 'tell me about Microsoft'\n" +
+        "* 'how many companies has apple bought?'\n" +
+        "* 'when did amazon go public?'\n" +
+        "* 'where is googles headquarters?'\n" +
+        "* 'who founded microsoft?'\n" +
+        "* 'what is amazons website?'\n" +
+        "\nOnce you ask a question about a company you can ask followup questions about the same company.");
         next();
       },
       builder.DialogAction.send('I\'m just a bot. I don\'t know everything!')
