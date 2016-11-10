c9eae382a0684d2d5b11e5f0c6c91ec0d0138341
Zui Young LIM <lim_zui_young@ida.gov.sg>
2016-11-10 09:50:41 +0800

    Get last element in currency detection.

--- app.js
+++ app.js
@@ -96,12 +96,12 @@ bot.dialog('/uploadImage', [
               });
             });
 
-            var currencyAmount = ocrText.match(/((([A-Z]{2,3}|TOTAL|Total|total|^)\s?(DUE|Due)*(:|\s)*)|(\$\s*))(\d,?.?)+.?\d*/g);
+            var currencyAmount = ocrText.match(/((([A-Z]{2,3}|BALANCE|Balance|TOTAL|Total|total|^)\s?(DUE|Due)*(:|\s)*)|(\$\s*))(\d,?.?)+.?\d*/g);
             var others = ocrText;
 
             if (currencyAmount !== null) {
               telegramDebug.notify(ocrText);
-              session.endDialog("I have added your invoice of " + currencyAmount+ " .");
+              session.endDialog("I have added your invoice of " + currencyAmount.split(/[, ]+/).pop() + " .");
 
             } else {
               session.send("I couldn't read your document, please send a clearer image.");
