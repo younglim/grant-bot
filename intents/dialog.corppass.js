f214fd57f0123671a1975f035fd42055882d9cca
Kavan Koh <kavanohk@gmail.com>
2016-11-10 11:10:58 +0800

    Update intents

--- intents/dialog.corppass.js
+++ intents/dialog.corppass.js
@@ -2,21 +2,13 @@ function getFlow(builder) {
   return [
     function(session, args, next) {
       session.send('CorpPass or Singapore Corporate Access is a secure way for your business to transact online with the goverment. To apply for a grant, you need a CorpPass account.');
-      builder.Prompts.choice(session, 'Does your company have a CorpPass administrator?', 'Yes|No');
+      builder.Prompts.confirm(session, 'Does your company have a CorpPass administrator?');
     },
     function(session, result, next) {
-      console.log(result);
-      console.log('process input');
-      switch (result.response.index) {
-        case 0:
-          session.send('yes');
-          break;
-        case 1:
-          session.send('no');
-          break;
-        default:
-          session.endDialog();
-          break;
+      if (result.response) {
+          session.send('Get your CorpPass account for you and assign an administrator to create an appropriate Business Grants Portal e-Service role. Then, simply log in to Business Grants Portal with your CorpPass account.');
+      } else {
+        session.send("Find out how to appoint a CorpPass administrator for your company here: https://www.corppass.gov.sg/cpauth/login/homepage?TAM_OP=login");
       }
       next();
     },
