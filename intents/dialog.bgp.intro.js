99cc7b36829c61c9b0d35370cf1b078fcfdb2e73
Kavan Koh <kavanohk@gmail.com>
2016-11-10 10:35:59 +0800

    Update bgp intro intent and add corppass intent

--- intents/dialog.bgp.intro.js
+++ intents/dialog.bgp.intro.js
@@ -2,15 +2,13 @@ function getFlow(builder) {
   return [
     function(session, args, next) {
       session.send('Business Grants Portal brings government grants for businesses into one place, so it\'s easier to find and apply for the grants you need. The grant support you receive will depend on your situation.');
-      next();
-    },
-    function(session, args) {
       builder.Prompts.choice(session, 'Would you like to know how to apply for a grant?', 'Yes|No');
     },
     function(session, result, next) {
       switch (result.response.index) {
         case 0:
           session.send('CorpPass or Singapore Corporate Access is a secure way for your business to transact online with the goverment. To apply for a grant, you need a CorpPass account.');
+          session.beginDialog('/corp-pass');
           break;
         case 1:
           session.send('What else can I help with today?');
--- intents/dialog.corppass.js
+++ intents/dialog.corppass.js
@@ -1,12 +1,35 @@
+function getFlow(builder) {
+  return [
+    function(session, args, next) {
+      session.send('CorpPass or Singapore Corporate Access is a secure way for your business to transact online with the goverment. To apply for a grant, you need a CorpPass account.');
+      builder.Prompts.choice(session, 'Does your company have a CorpPass administrator?', 'Yes|No');
+    },
+    function(session, result, next) {
+      console.log(result);
+      console.log('process input');
+      switch (result.response.index) {
+        case 0:
+          session.send('yes');
+          break;
+        case 1:
+          session.send('no');
+          break;
+        default:
+          session.endDialog();
+          break;
+      }
+      next();
+    },
+    function(session) {
+      session.endDialog();
+    }
+  ];
+}
+
 module.exports = {
   label: 'CorpPass Intro',
-  callbackProvider: (builder) => {
-    return [
-      function(session, args, next) {
-        session.send('1');
-        next();
-      },
-      builder.DialogAction.send('CorpPass or Singapore Corporate Access is a secure way for your business to transact online with the goverment. To apply for a grant, you need a CorpPass account. Does your company have a CorpPass administrator?')
-    ];
+  callbackProvider: (builder, bot) => {
+    bot.dialog('/corp-pass', getFlow(builder));
+    return getFlow(builder);
   }
 };
