72ea9522ee95bd5c6f24f0eba3b18e70b7b5aaad
Joseph Matthias Goh <govtech@joeir.net>
2016-11-11 10:26:55 +0800

    Making test joseph shorter.

--- intents/test.joseph01.js
+++ intents/test.joseph01.js
@@ -4,32 +4,11 @@ module.exports = {
   label: 'Test Joseph',
   callbackProvider: (builder) => {
     return [
-      function(session, args, next) {
+      function(session, args) {
         const User = require('../users');
         User.pushUser(session.message.address);
-        next();
-      },
-      function(session, args, next) {
-        builder.Prompts.text(session, 'Asking a question! Type in your answer:');
-      },
-      function(session, result, next) {
-        session.send(`You entered: ${result.response}`);
-        next();
-      },
-      function(session, args, next) {
-        builder.Prompts.choice(session, '😤 Make a choice!', 'Trump|Hillary|Obama');
-      },
-      function(session, result, next) {
-        console.log(result);
-        session.send(`Great work, you chose: ${result.response.entity}!!`);
-        switch(result.response.index) {
-          case 0: session.send('No one, NO one, knows bots like I do.'); break;
-          case 1: session.send('MAKE YOUR CHOICE!!! MAKE YOUR *coughs*'); break;
-          case 2: session.send('Aight my niggas, you didn\'t like Obamacare'); break;
-        }
-        next();
-      },
-      builder.DialogAction.send('Label:Test Joseph ends here')
+        session.send('Registered user ' + JSON.stringify(session.message.address));
+      }
     ];
   }
 };
